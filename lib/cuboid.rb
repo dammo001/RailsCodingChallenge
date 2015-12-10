8e87c718ecd68a54bdecf7e2dc6e740e2424359a
David Ammons <dammo001@gmail.com>
2015-12-09 18:05:32 -0800

    all cuboid challenge except rotations

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,56 @@
 
 class Cuboid
-  
-  #BEGIN public methods that should be your starting point
 
-  def move_to!(x, y, z)
-  end
-  
-  def vertices
-  end
-  
-  #returns true if the two cuboids intersect each other.  False otherwise.
-  def intersects?(other)
-  end
+	attr_reader :vertices, :origin, :top, :bottom, :left, :right, :front, :back 
 
-  #END public methods that should be your starting point  
+	def initialize(origin, length, width, height)
+		@origin = origin
+		@length = length
+		@width = width
+		@height = height 
+		find_sides(length, width, height) 
+		find_vertices  
+	end
+
+	def move_to!(origin) 
+		@origin = origin 
+		find_sides(@length, @width, @height) 
+		find_vertices
+	end
+
+#returns true if the two cuboids intersect each other.  False otherwise.
+	def intersects?(other) 
+		return not( 
+			@top < other.bottom ||
+	        @bottom > other.top ||
+	        @left > other.right ||
+	        @right < other.left ||
+	        @front < other.back ||
+	        @back > other.front 
+        )  
+	end
+
+	private 
+
+	def find_sides(length, width, height) 
+		x = @origin[0]
+		y = @origin[1]
+		@top = y + height/2.0
+		z = @origin[2]
+		@bottom = y - height/2.0
+		@left = x - width/2.0
+		@right = x + width/2.0
+		@front = z + length/2.0
+		@back = z - length/2.0
+	end
+
+	def find_vertices 
+		@vertices = [
+			[@left, @bottom, @front], [@right, @bottom, @front], 
+			[@left, @bottom, @back], [@right, @bottom, @back], 
+			[@left, @top, @front], [@right, @top, @front], 
+			[@left, @top, @back], [@right, @top, @back]
+		] 
+	end
+   
 end
